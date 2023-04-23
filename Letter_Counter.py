4819c09ad887d963355a78d093ef690b2a23a579
Stan <dimitrov.s.dev@gmail.com>
2023-04-23 16:42:45 +0300

    Letter_Counter.py

--- Letter_Counter.py
+++ Letter_Counter.py
@@ -2,5 +2,5 @@ import requests
 
 response = requests.get("http://www.pythonhow.com/data/universe.txt")
 text = response.text
-count_of_a = text.count("a")
-print(count_of_a)
+count_a = text.count("a")
+print(count_a)
