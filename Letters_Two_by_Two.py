17bf9d515c22b4eeaff19b3f0dc7460638f4802c
Stan <dimitrov.s.dev@gmail.com>
2023-04-23 14:30:58 +0300

    Letters_Two_by_Two

--- Letters_Two_by_Two.py
+++ Letters_Two_by_Two.py
@@ -3,7 +3,7 @@ import string
 word_one = string.ascii_lowercase[::2]
 word_two = string.ascii_lowercase[1::2]
 
-for a, b in zip(word_one, word_two):
-    text = f"{a}{b}"
+for one, two in zip(word_one, word_two):
+    text = f"{one}{two}"
     with open("result.txt", "a") as file:
         file.write(f"{text}" + "\n")
