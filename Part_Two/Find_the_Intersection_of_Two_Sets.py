7fb83a0addf88c93ce969815cb6b9392ad83df2d
Stan <dimitrov.s.dev@gmail.com>
2023-04-25 13:44:54 +0300

    Part_Two

---
+++ Part_Two/Find_the_Intersection_of_Two_Sets.py
@@ -0,0 +1,12 @@
+"""
+Write a Python program that finds the intersection of two sets (set1 and set2).
+Create a new set called intersection with their intersection.
+Print the new set as the output.
+If the intersection is empty, print an empty set.
+"""
+
+
+set1 = {1, 2, 3}
+set2 = {4, 5, 6}
+
+print(set1 & set2)
