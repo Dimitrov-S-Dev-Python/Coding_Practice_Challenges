01bad9864429cfc29a91a8906c94c677f3c60bd7
Stan <dimitrov.s.dev@gmail.com>
2023-04-25 17:47:56 +0300

    Part_Two

---
+++ Part_Two/Make_a_Dictionary_from_Nested_Lists.py
@@ -0,0 +1,17 @@
+"""
+Write a Python program that creates a dictionary from the values
+contained in nested lists.
+Each nested list has this format [value1, value2].
+value1 should be the key in the dictionary and value2 should be its
+corresponding value.
+If there are no nested lists, print an empty dictionary.
+"""
+my_list = [["a", 1], ["b", 2], ["c", 3], ["d", 4]]
+my_dict = {}
+
+for element in my_list:
+    key = element[0]
+    value = element[1]
+    my_dict[key] = value
+
+print(my_dict)
