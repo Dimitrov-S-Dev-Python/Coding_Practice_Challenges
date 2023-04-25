909e9ab97bcd90d44ff422bc8e2677601d8d3627
Stan <dimitrov.s.dev@gmail.com>
2023-04-25 13:34:53 +0300

    Part_Two

---
+++ Part_Two/Multiply_all_Elements_in_a_List.py
@@ -0,0 +1,13 @@
+"""
+Write a Python program that multiplies all the items in a list
+by the value of the variable factor.
+The program must print the list as the output.
+The program should also allow multiplying the variable factor
+by a string in case the list contains strings.
+You may assume that the value of factor will be a positive integer.
+"""
+
+list_items = [3, 4, 5, "a"]
+factor = int(input())
+result = [x * factor for x in list_items]
+print(result)
