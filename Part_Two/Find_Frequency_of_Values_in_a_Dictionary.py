6cc1cd20006c0bd710e0a2a86599e8680a58a290
Stan <dimitrov.s.dev@gmail.com>
2023-04-25 17:42:13 +0300

    Part_Two

---
+++ Part_Two/Find_Frequency_of_Values_in_a_Dictionary.py
@@ -0,0 +1,26 @@
+"""
+Write a Python program that counts the frequency of each value in a dictionary.
+The program should create a new dictionary to map each value in the
+original dictionary to its frequency (how many times it occurs).
+If the dictionary is empty, print an empty dictionary.
+"""
+my_dict ={
+    "a":4,
+    "b":4,
+    "c":2,
+    "d":7,
+    "e":4,
+    "f":2,
+    "g":7,
+    "h":7
+}
+
+if my_dict:
+    freq_dict = {}
+    for v in my_dict.values():
+        if v not in freq_dict:
+            freq_dict[v] = 0
+        freq_dict[v] += 1
+    print(freq_dict)
+else:
+    print("Empty")
