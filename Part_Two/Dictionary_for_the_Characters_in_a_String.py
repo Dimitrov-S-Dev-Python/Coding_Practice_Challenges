ae31c6b045196c254d2764ea08626b5933c07677
Stan <dimitrov.s.dev@gmail.com>
2023-04-25 21:49:28 +0300

    Part_Two

---
+++ Part_Two/Dictionary_for_the_Characters_in_a_String.py
@@ -0,0 +1,21 @@
+"""
+Write a program that creates and displays a dictionary that maps
+each letter in a string to how many times the character occurs
+in the string (its frequency).
+The dictionary should only include the characters in the string.
+The test should be case-insensitive ("A" should be counted as "a").
+The keys in the dictionary should be lowercase letters.
+Only include letters in the dictionary.
+"""
+
+my_string = "Hello, World"
+result = {}
+
+for element in my_string.lower():
+    if not element.isalpha():
+        continue
+    if element not in result.keys():
+        result[element] = 0
+    result[element] += 1
+
+print(result)
