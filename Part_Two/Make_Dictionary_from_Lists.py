7c65061fcca42eed10234793124b1aca6bd7f0ce
Stan <dimitrov.s.dev@gmail.com>
2023-04-25 14:02:49 +0300

    Part_Two

---
+++ Part_Two/Make_Dictionary_from_Lists.py
@@ -0,0 +1,11 @@
+"""
+Write a Python program that creates and print a dictionary that maps
+each element in a list to its corresponding frequency
+(how many times it occurs in the list).
+The test should be case-sensitive. Therefore, "A" should not be considered
+the same element as "a"
+"""
+
+list_items = ["a", "a", "b", "c", "a", "b"]
+result = {k: list_items.count(k) for k in list_items}
+print(result)
