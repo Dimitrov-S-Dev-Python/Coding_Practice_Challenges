2b4cfb88d71f87c7783cb71948faaa00371cc83c
Stan <dimitrov.s.dev@gmail.com>
2023-04-26 16:24:43 +0300

    Part_Three

---
+++ Part_Three/Intersection.py
@@ -0,0 +1,11 @@
+"""
+Write a function called intersection. This function should accept two lists
+and return a list with the values that are in both input lists.
+intersection([1,2,3], [2,3,4])    #[2,3]
+intersection(['a','b','z'], ['x','y','z']) .  # ['z']
+"""
+
+def intersection(collection1, collection2):
+    return [x for x in collection1 if x in collection2]
+
+print(intersection([1,2,3], [2,3,4]))
