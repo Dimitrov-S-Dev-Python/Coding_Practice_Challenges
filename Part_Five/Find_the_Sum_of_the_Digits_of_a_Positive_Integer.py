1ca4944d1c3403a8463d3c9e215a75899f642287
Stan <dimitrov.s.dev@gmail.com>
2023-04-26 20:14:14 +0300

    Part_Three

---
+++ Part_Five/Find_the_Sum_of_the_Digits_of_a_Positive_Integer.py
@@ -0,0 +1,14 @@
+"""
+Write a program that calculates and prints the sum of the digits of a positive
+integer num.
+The program must find the sum recursively.
+If the integer has only one digit, print the integer as the total sum.
+"""
+
+def sum_of_digits(number):
+    if number == 0:
+        return 0
+    else:
+        return number % 10  + sum_of_digits(number // 10)
+
+print(sum_of_digits(10))
