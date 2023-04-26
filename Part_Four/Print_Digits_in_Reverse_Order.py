e07a9dbef0fbeecef9f2e7a128e0612c6964ee76
Stan <dimitrov.s.dev@gmail.com>
2023-04-26 12:33:15 +0300

    Part_Four

---
+++ Part_Four/Print_Digits_in_Reverse_Order.py
@@ -0,0 +1,9 @@
+"""
+Write a program that prints the  number in reverse order on the same line.
+If the number only has one digit, print this digit
+"""
+
+num_str = input()
+
+reverse = int(num_str[::-1])
+print(reverse)
