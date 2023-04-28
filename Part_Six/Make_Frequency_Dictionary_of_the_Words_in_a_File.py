a81201e4d17709bc3097d81d63584d9afedb338c
Stan <dimitrov.s.dev@gmail.com>
2023-04-28 17:01:38 +0300

    Part_Six

--- Part_Six/Make_Frequency_Dictionary_of_the_Words_in_a_File.py
+++ Part_Six/Make_Frequency_Dictionary_of_the_Words_in_a_File.py
@@ -6,9 +6,20 @@ The values should be their frequencies.
 You may assume that each line of the file only contains one word.
 """
 
-with open("words.txt", "r") as file:
-    content = file.readlines()
+# with open("words.txt", "r") as file:
+#     content = file.readlines()
+#
+# words = [word.strip("\n") for word in content]
+# words_dict = {word:words.count(word) for word in words}
+# print(words_dict)
 
-words = [word.strip("\n") for word in content]
-words_dict = {word:words.count(word) for word in words}
+words_dict = {}
+with open("words.txt", "r") as file:
+    for word in file:
+        word = word.strip("\n")
+        if word not in words_dict.keys():
+            words_dict[word] = 0
+        words_dict[word] += 1
+
 print(words_dict)
+
