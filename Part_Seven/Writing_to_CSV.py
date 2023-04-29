7871c16e4da66a2544c8c302e55a58795c514d81
Stan <dimitrov.s.dev@gmail.com>
2023-04-29 16:03:12 +0300

    Part_Seven

--- Part_Seven/Writing_to_CSV.py
+++ Part_Seven/Writing_to_CSV.py
@@ -1,7 +1,24 @@
-from csv import writer
+# from csv import writer
+# write a new file
 
-with open("test.csv", "w") as file:
+# with open("test.csv", "w") as file:
+#     data = writer(file)
+#     data.writerow(["first_name", "last_name"])
+#     data.writerow(["Sancho", "Pansa"])
+#     data.writerow(["Carabas", "Barabas"])
+#
+
+# open and write existing
+
+from csv import reader, writer
+
+with open("fighters.csv", "r") as file:
+    content = reader(file)
+    info = [[s.upper() for s in row ] for row in content]
+
+
+with open("test.csv", "w", newline="") as file:
     data = writer(file)
-    data.writerow(["first_name", "last_name"])
-    data.writerow(["Sancho", "Pansa"])
-    data.writerow(["Carabas", "Barabas"])
+    for header in info:
+        data.writerow(header)
+
