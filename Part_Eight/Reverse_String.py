13f303a6d6ccfb4c0a0ddf149c911cac81b3d7b3
Stan <dimitrov.s.dev@gmail.com>
2023-04-29 17:27:11 +0300

    Part_Seven

---
+++ Part_Eight/Reverse_String.py
@@ -0,0 +1,10 @@
+"""
+reverse_string
+Write a function called reverse_string which accepts a string and returns a
+new string with all the characters reversed.
+"""
+
+def reverse_string(text):
+    return text[::-1]
+
+print(reverse_string("Elie"))
