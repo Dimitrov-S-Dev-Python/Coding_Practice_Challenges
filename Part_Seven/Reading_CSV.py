3da10b4e64bfba41c65eb29be9f376c8aacbc2dc
Stan <dimitrov.s.dev@gmail.com>
2023-04-29 14:54:49 +0300

    Part_Seven

--- Part_Seven/Reading_CSV.py
+++ Part_Seven/Reading_CSV.py
@@ -1,7 +1,15 @@
 from csv import reader
 
+# with open("fighters.csv", "r") as file:
+#     content = reader(file)
+#     next(content)
+#     for fighter in content:
+#         print(f"{fighter[0]} is from {fighter[1]}")
+
+# or
+
 with open("fighters.csv", "r") as file:
     content = reader(file)
-    next(content)
-    for fighter in content:
-        print(f"{fighter[0]} is from {fighter[1]}")
+    data = list(content)
+    for name, country, _ in data[1:]:
+        print(f"{name} is from {country}")
