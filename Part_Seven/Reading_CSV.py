9876451124dfa552ff6dbf16dba20cd7952a1ca4
Stan <dimitrov.s.dev@gmail.com>
2023-04-29 15:00:58 +0300

    Part_Seven

--- Part_Seven/Reading_CSV.py
+++ Part_Seven/Reading_CSV.py
@@ -1,4 +1,4 @@
-from csv import reader
+# from csv import reader
 
 # using reader
 
@@ -10,8 +10,19 @@ from csv import reader
 
 # or
 
+# with open("fighters.csv", "r") as file:
+#     content = reader(file)
+#     data = list(content)
+#     for name, country, _ in data[1:]:
+#         print(f"{name} is from {country}")
+
+#--------------------------------------------
+
+# using DictReader -> OrderedDict
+
+from csv import DictReader
+
 with open("fighters.csv", "r") as file:
-    content = reader(file)
-    data = list(content)
-    for name, country, _ in data[1:]:
-        print(f"{name} is from {country}")
+    content = DictReader(file)
+    for row in content:
+        print(f"{row['Name']} is from {row['Country']}")
