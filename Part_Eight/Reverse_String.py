14cce6eb77a7c4eebc2651b5e686b52d30fbb802
Stan <dimitrov.s.dev@gmail.com>
2023-04-29 18:09:04 +0300

    Part_Eight

--- Part_Eight/Reverse_String.py
+++ Part_Eight/Reverse_String.py
@@ -1,9 +1,12 @@
 """
 Write a function called reverse_string which accepts a string and returns a
 new string with all the characters reversed.
+reverse_string('awesome') # 'emosewa'
+reverse_string('Colt') # 'tloC'
+reverse_string('Elie') # 'eilE'
 """
 
 def reverse_string(text):
     return text[::-1]
 
-print(reverse_string("Elie"))
+print(reverse_string("awesome"))
