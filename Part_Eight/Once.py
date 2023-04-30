bde741d37320cf4df443c414ffd13067b390b8c4
Stan <dimitrov.s.dev@gmail.com>
2023-04-30 23:19:11 +0300

    Part_Eight

--- Part_Eight/Once.py
+++ Part_Eight/Once.py
@@ -13,3 +13,14 @@ oneAddition(2,2) # 4
 oneAddition(2,2) # None
 oneAddition(12,200) # None
 """
+
+
+def once(fn):
+    fn.is_called = False
+
+    def inner(*args):
+        if not fn.is_called:
+            fn.is_called = True
+            return fn(*args)
+
+    return inner
