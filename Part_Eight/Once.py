a211812aeab4c6961cac5166a24598f69d2ae76b
Stan <dimitrov.s.dev@gmail.com>
2023-04-30 23:18:43 +0300

    Part_Eight

--- Part_Eight/Once.py
+++ Part_Eight/Once.py
@@ -4,4 +4,12 @@ This function accepts a function and returns a new function that can only be inv
 If the function is invoked more than once, it should return None.
 Hint you will need to define a new function inside your once function and return that function.
 You can add properties to your inner function to see if it has run already.
+def add(a,b):
+    return a+b
+
+oneAddition = once(add)
+
+oneAddition(2,2) # 4
+oneAddition(2,2) # None
+oneAddition(12,200) # None
 """
