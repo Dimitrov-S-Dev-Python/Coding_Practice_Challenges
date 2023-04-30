7ae4f1cca01094164f4f8888b1d3d1ff436890e1
Stan <dimitrov.s.dev@gmail.com>
2023-04-30 23:02:37 +0300

    Part_Eight

---
+++ Part_Eight/Running_Average.py
@@ -0,0 +1,3 @@
+"""
+Create a function running_average that returns a function. When the function returned is passed a value, the function returns the current average of all previous function calls. You will have to use closure to solve this. You should round all answers to the 2nd decimal place.
+"""
