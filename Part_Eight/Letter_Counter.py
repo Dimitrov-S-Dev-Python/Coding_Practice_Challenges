d765ec6c9814522c5f92639fcfd5ed097f790a09
Stan <dimitrov.s.dev@gmail.com>
2023-04-30 23:12:03 +0300

    Part_Eight

--- Part_Eight/Letter_Counter.py
+++ Part_Eight/Letter_Counter.py
@@ -3,4 +3,11 @@ Write a function called letter_counter which accepts a string and returns a func
 When the inner function is invoked it should accept a parameter which is a letter,
 and the inner function should return the number of times that letter appears.
 This inner function should be case-insensitive.
+counter = letter_counter('Amazing')
+counter('a') # 2
+counter('m') # 1
+
+counter2 = letter_counter('This Is Really Fun!')
+counter2('i') # 2
+counter2('t') # 1
 """
